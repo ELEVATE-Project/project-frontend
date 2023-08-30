a2d8411e61e3ab9694aaa62ff1e13e00d7d76ea4
Abhishek <bhattabhi013@gmail.com>
2023-08-30 14:21:24 +0530

    fixed design issues

--- src/app/pages/global-search/global-search.page.ts
+++ src/app/pages/global-search/global-search.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { urlConstants } from 'src/app/core/constants';
+import { headerConfigKeys, urlConstants } from 'src/app/core/constants';
 import { HttpService, ToastService } from 'src/app/core/services';
+import { UtilService } from 'src/app/shared/util.service';
 
 @Component({
   selector: 'app-global-search',
@@ -10,49 +11,75 @@ import { HttpService, ToastService } from 'src/app/core/services';
 export class GlobalSearchPage implements OnInit {
 
   searchResults: any[] = [];
-  debounceTimer: any;
   currentPage = 1;
+  isLoading = false;
+  debounceTimer: any;
   limit = 2;
+  public title: string = 'GLOBAL_SEARCH.TITLE'
+  searchTerm: any = "";
 
   constructor(
     private http: HttpService,
     private toast: ToastService,
+    private utilsService: UtilService
     ) {}
   ngOnInit(): void {
-    this.loadSearchResults("");
+    this.getProjects();
+    this.utilsService.setHeaders({
+      [headerConfigKeys.SHOW_ICON]: true,
+      [headerConfigKeys.SHOW_MENU]: true,
+      [headerConfigKeys.SHOW_SEARCH]: false,
+      [headerConfigKeys.SHOW_NOTIFICATION]: false,
+      [headerConfigKeys.SHOW_PROFILE]: true,
+      })
   }
 
   onSearch(event: any) {
-    const query = event.target.value.trim();
+    this.searchTerm = event.target.value.trim();
     
-    if (query.length >= 3) {
+    if (this.searchTerm.length >= 3) {
       this.currentPage = 1;
 
       // Debounce the API call to avoid frequent requests while typing
       clearTimeout(this.debounceTimer);
       this.debounceTimer = setTimeout(() => {
-        this.loadSearchResults(query);
+        this.getProjects();
       }, 300);
     }
   }
 
-  async loadSearchResults(query: string) {
-    const dynamicUrl = urlConstants.API_URLS.GLOBAL_SEARCH(this.currentPage, this.limit, query);
+  async getProjects() {
+    const dynamicUrl = urlConstants.API_URLS.GLOBAL_SEARCH(this.currentPage, this.limit, this.searchTerm);
     const config = {
       url: dynamicUrl,
     };
+
     await this.http.setHeader();
     this.http.get(config).subscribe((data) => {
+      this.isLoading = false;
       if (data) {
         if(data.result == 0){
           this.toast.showToast('No Data found', 'success');
           return;
         }
         // Append new results to the existing list
-        this.searchResults = data.result;
+         this.searchResults = this.searchResults.concat(data.result);
+        this.currentPage++;
         
       }
     });
+  }
+
+
+  loadData(event: any) {
+    if (this.isLoading) {
+      return;
+    }
+    this.isLoading = true;
+    this.getProjects();
+    if (event) {
+      event.target.complete();
+    }
   }
 
 }
