84bb70f98d39737a7744ebf850357615acd57fae
Abhishek <bhattabhi013@gmail.com>
2023-09-06 02:50:53 +0530

    alert service

---
+++ src/app/core/services/alert/alert.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AlertService {
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert(message: string): Promise<boolean> {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      message,
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          handler: () => {
+            alert.dismiss(false);
+            return false;
+          },
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            alert.dismiss(true);
+            return false;
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+
+    const result = await alert.onDidDismiss();
+
+    return result.data as boolean;
+  }
+}
